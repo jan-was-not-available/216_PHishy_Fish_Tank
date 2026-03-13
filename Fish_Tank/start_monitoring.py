58cf6ef2b384c5fd13d741bc945c5a589846308b
jan-was-not-available <alejandro.oliveroasencio@stu.vertexacademies.org>
2026-03-13 18:18:41 +0000

    Minor Fixes

--- Fish_Tank/start_monitoring.py
+++ Fish_Tank/start_monitoring.py
@@ -1,4 +1,4 @@
 # start_monitoring.py
-# from fishtank import FishTankCLI
-# app = FishTankCLI()
-# app.main()
+from fishtank import FishTankCLI #import the class 
+app = FishTankCLI()              #create an instance of fishtankCLI
+app.main()                       #run the main function
